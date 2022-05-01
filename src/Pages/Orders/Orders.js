99937516d22d2a36f3fe5462c36b5e8dfb656cc1
proalamin-247 <pro.alamin247@gmail.com>
2022-05-01 16:46:40 +0600

    orders page show author ordes deatils

--- src/Pages/Orders/Orders.js
+++ src/Pages/Orders/Orders.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
 
 const Orders = () => {
+    const [user] = useAuthState(auth);
+    const [orders, setOrders]= useState([]);
+    useEffect(()=>{
+
+        const getOrders = async()=>{
+            const email = user.email;
+            const url = `http://localhost:5000/orders?email=${email}`;
+            const {data}= await axios.get(url);
+            setOrders(data);
+        }
+        getOrders();
+
+    },[])
+
     return (
         <div>
-            <h1>All Orders</h1>
+            <h1>All Orders: {orders.length}</h1>
         </div>
     );
 };
